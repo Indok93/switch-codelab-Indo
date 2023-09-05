ab6c234ef87ba280e4e0581ceea269a327fd88f2
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-09-05 10:51:58 +0200

    easy opgaver done
    underlige errors undervejs

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -10,12 +10,12 @@ let myAnswerFeedbackElement = document.getElementById('answerFeedbackOne');
 
 myAnswerButton.addEventListener('click', (event) => {
     event.preventDefault();
-    // console.warn('opgave 1 og 2');
+    console.warn('opgave 1 og 2');
     /* din kode her.*/
 
     // henter værdi af inputfeltet
     const input = (myAnswerInput.value);
-    // console.log(input);
+    // console.log('input', input);
 
     switch (input) {
         case '1':
@@ -53,40 +53,139 @@ function showMessage(myMessage) {
 
 
 // opgave 3 og 4
- myAnsverInput = document.getElementById('answerTwo');
- myAnsverButton = document.getElementById('answerButtonTwo');
+ myAnswerInput = document.getElementById('InputTwo');
+ myAnswerButton = document.getElementById('answerButtonTwo');
 
- myAnsverFeedbackElement = document.getElementById('answerFeedbackTwo');
+ myAnswerFeedbackElement = document.getElementById('answerFeedbackTwo');
 
- myAnsverButton.addEventListener('click', (event) => {
+ myAnswerButton.addEventListener('click', (event) => {
     event.preventDefault();
     console.warn('opgave 3 og 4');
     /* din kode her.*/
+
+    // henter værdi af inputfeltet
+    const input = (myAnswerInput.value);
+    // console.log(input);
+    switch (input) {
+        case '1':
+            // console.log('Januar');
+            showMessagetwo('Januar')
+            break;
+        case '2':
+            // console.log('Februar');
+            showMessagetwo('Februar')
+            break;
+        case '3':
+            // console.log('Marts');
+            showMessagetwo('Marts')
+            break;
+        case '4':
+            // console.log('April');
+            showMessagetwo('April')
+            break;
+        case '5':
+            // console.log('Maj');
+            showMessagetwo('Maj')
+            break;
+        case '6':
+            // console.log('Juni');
+            showMessagetwo('Juni')
+            break;
+        case '7':
+            // console.log('Juli');
+            showMessagetwo('Juli')
+            break;
+        case '8':
+            // console.log('August');
+            showMessagetwo('August')
+            break;
+        case '9':
+            // console.log('September');
+            showMessagetwo('September')
+            break;
+        case '10':
+            // console.log('Oktober');
+            showMessagetwo('Oktober')
+            break;
+        case '11':
+            // console.log('November');
+            showMessagetwo('November')
+            break;
+        case '12':
+            // console.log('December');
+            showMessagetwo('December')
+            break;
+        default:
+            // console.warn('Dette er ikke en gyldig måned');
+            showMessagetwo('Dette er ikke en gyldig måned')
+            break;
+    }
 });
 
 /* din view function her */
-
+function showMessagetwo(myMessage) {
+    myAnswerFeedbackElement.innerHTML = `<h4>${myMessage}</h4>`;
+}
 
 // opgave 5
-myAnsverInput = document.getElementById('InputFive');
-myAnsverButton = document.getElementById('answerButtonFive');
+myAnswerInput = document.getElementById('InputFive');
+myAnswerButton = document.getElementById('answerButtonFive');
 
-myAnsverFeedbackElement = document.getElementById('answerFeedbackFive');
+myAnswerFeedbackElement = document.getElementById('answerFeedbackFive');
 
-myAnsverButton.addEventListener('click', (event) => {
+myAnswerButton.addEventListener('click', (event) => {
    event.preventDefault();
 
    console.warn('opgave 5');
 
-   workDays(myAnsverInput.value)
+   workDays(myAnswerInput.value)
   
 });
 
 
  /* din kode her.*/
+function workDays(string) {
+    myAnswerFeedbackElement.innerHTML = "";
 
+    switch (string) {
+        case 'mandag':
+            // console.log('Arbejdsdag');
+            showMessageFive('Arbejdsdag');
+            break;
+        case 'tirsdag':
+            // console.log('Arbejdsdag');
+            showMessageFive('Arbejdsdag');
+            break;
+        case 'onsdag':
+            // console.log('Arbejdsdag');
+            showMessageFive('Arbejdsdag');
+            break;
+        case 'torsdag':
+            // console.log('Arbejdsdag');
+            showMessageFive('Arbejdsdag');
+            break;
+        case 'fredag':
+            // console.log('Arbejdsdag');
+            showMessageFive('Arbejdsdag');
+            break;
+        case 'lørdag':
+            // console.log('weekendag');
+            showMessageFive('Weekenddag');
+            break;
+        case 'søndag':
+            // console.log('weekenddag');
+            showMessageFive('weekenddag');
+            break;
+        default:
+            // console.log('Du har ikke indtastet en uge dag');
+            showMessageFive('Du har ikke intastet en uge dag');
+            break;
+    }
+}
 
 
 /* din view function her */
-
+function showMessageFive(myMessage) {
+    myAnswerFeedbackElement.innerHTML = `<h4>${myMessage}</h4>`;
+}
 
