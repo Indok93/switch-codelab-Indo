a60d7983d79e17098b43877bad11e897db61f39a
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-09-05 11:51:15 +0200

    update hard

--- opgavefiler/js/hard.js
+++ opgavefiler/js/hard.js
@@ -4,11 +4,39 @@
 
 
 // opgave 1 her
+const myamount = document.getElementById('amountInput');
+const myTransactionType = document.getElementById('myTransaction');
+const mySaldo = document.getElementById('accountSaldo');
 
 
+myTransactionType.addEventListener('change', (event) => {
+   event.preventDefault();
 
+   // tjek om change ændre sig
+   // console.log(myTransactionType.value);
+   
 
+   // lav en switch der kalder transaktioner fra change event
+   switch (myTransactionType.value) {
+      case 'deposit':
+         console.log('deposit here');
+         break;
+      case 'withdraw':
+         console.log('withdraw here');
+         break;
+      case 'saldo':
+         console.log('check saldo');
+         break;
+   }
 
+});
+
+/* 
+brug en "global" variabel til at gemme saldi i.
+brug en switch statement til at kalde
+forskellige transaktioner fra change event
+se evt. if then codelab knallert øvelse 
+*/
 
 
 
