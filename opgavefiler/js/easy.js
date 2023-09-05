d0976764966ee3177648d95aaf642bba514d6958
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-09-05 10:06:35 +0200

    easy opgaver 1 og 2 færdige

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,21 +1,55 @@
-let myAnsverInput = document.getElementById('answerOne');
-let myAnsverButton = document.getElementById('answerButtonOne');
+let myAnswerInput = document.getElementById('InputOne');
+let myAnswerButton = document.getElementById('answerButtonOne');
 
-let myAnsverFeedbackElement = document.getElementById('answerFeedback');
+let myAnswerFeedbackElement = document.getElementById('answerFeedbackOne');
 
 
 
 
 // opgave 1 og 2
 
-myAnsverButton.addEventListener('click', (event) => {
+myAnswerButton.addEventListener('click', (event) => {
     event.preventDefault();
-    console.warn('opgave 1 og 2');
+    // console.warn('opgave 1 og 2');
     /* din kode her.*/
+
+    // henter værdi af inputfeltet
+    const input = (myAnswerInput.value);
+    // console.log(input);
+
+    switch (input) {
+        case '1':
+            // console.log('du har valgt: ' + input);
+            showMessage('du har valgt: ' + input);
+            break;
+        case '2':
+            // console.log('du har valgt: ' + input);
+            showMessage('du har valgt: ' + input);
+            break;
+        case '3':
+            // console.log('du har valgt: ' + input);
+            showMessage('du har valgt: ' + input);
+            break;
+        case '4':
+            // console.log('du har valgt: ' + input);
+            showMessage('du har valgt: ' + input);
+            break;
+        case '5':
+            // console.log('du har valgt: ' + input);
+            showMessage('du har valgt: ' + input);
+            break;
+        default:
+            showMessage('du har valgt et ugyldigt nummer: ' + input + ' - Vælg venligst et tal mellem 1 og 5');
+            // console.warn('du har valgt: ' + input + ' - Vælg venligst et tal mellem 1 og 5');
+            break;
+    }
+    
 });
 
 /* skriv evt en view funktion der viser om svaret er korrekt i myAnsverFeedbackElement, brug evt. innerHTML*/
-
+function showMessage(myMessage) {
+    myAnswerFeedbackElement.innerHTML = `<h4>${myMessage}</h4>`;
+}
 
 
 // opgave 3 og 4
